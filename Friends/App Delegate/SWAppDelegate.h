8cdcf41820e6daa2ad81471a0b245f627ed10236
Simon Westerlund <s@simonwesterlund.se>
2014-01-23 22:19:18 +0100

    Working tree

---
+++ Friends/App Delegate/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  Friends
+//
+//  Created by Simon Westerlund on 23/01/14.
+//  Copyright (c) 2014 Simon Westerlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Friends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Friends
+//
+//  Created by Simon Westerlund on 23/01/14.
+//  Copyright (c) 2014 Simon Westerlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
