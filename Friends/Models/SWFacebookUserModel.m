10684fffe2fecdcd64d70c5a6c9c61f94950550b
Simon Westerlund <simon.westerlund@blocket.se>
2014-01-26 15:49:39 +0100

    Set profile picture url from data and read the name display order

--- Friends/Models/SWFacebookUserModel.m
+++ Friends/Models/SWFacebookUserModel.m
@@ -11,12 +11,14 @@
 
 static NSString *const kFacebookAPIFirstName = @"first_name";
 static NSString *const kFacebookAPILastName = @"last_name";
+static NSString *const kFacebookAPIPictureUrl = @"last_name";
 
 @interface SWFacebookUserModel ()
 
 @property (nonatomic, strong, readwrite) NSString *firstName;
 @property (nonatomic, strong, readwrite) NSString *lastName;
 @property (nonatomic, strong, readwrite) NSURL *pictureUrl;
+@property (nonatomic, getter = isDisplayFormatFirstNameFirst) BOOL displayFormatFirstNameFirst;
 
 @end
 
@@ -26,12 +28,18 @@ static NSString *const kFacebookAPILastName = @"last_name";
     self = [super init];
     if (self) {
         
+        ABAddressBookCreateWithOptions(NULL, NULL);
+        [self setDisplayFormatFirstNameFirst:ABPersonGetCompositeNameFormatForRecord(NULL) == kABPersonCompositeNameFormatFirstNameFirst];
+        
         if ([dictionary isKindOfClass:[NSDictionary class]]) {
             if ([dictionary objectForKey:kFacebookAPIFirstName]) {
                 [self setFirstName:[dictionary objectForKey:kFacebookAPIFirstName]];
             }
             if ([dictionary objectForKey:kFacebookAPILastName]) {
                 [self setLastName:[dictionary objectForKey:kFacebookAPILastName]];
+            }
+            if ([[[dictionary objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]) {
+                [self setPictureUrl:[NSURL URLWithString:[[[dictionary objectForKey:@"picture"] objectForKey:@"data"] objectForKey:@"url"]]];
             }
         } else {
             *error = [NSError errorWithDomain:@"com.simonwesterlund.friends" code:160 userInfo:@{NSLocalizedDescriptionKey: @"no/invalid dictionary was provided"}];
