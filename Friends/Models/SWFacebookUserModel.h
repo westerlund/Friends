0cc20ef6f7e754dc2b1f93c66a0e4ee5389f120d
Simon Westerlund <simon.westerlund@blocket.se>
2014-01-26 13:01:38 +0100

    Created a name property to always return right display order

--- Friends/Models/SWFacebookUserModel.h
+++ Friends/Models/SWFacebookUserModel.h
@@ -10,6 +10,8 @@
 
 @interface SWFacebookUserModel : NSObject
 
+/// first name and last name combined with display order as address book
+@property (nonatomic, readonly) NSAttributedString *name;
 @property (nonatomic, readonly) NSString *firstName;
 @property (nonatomic, readonly) NSString *lastName;
 @property (nonatomic, readonly) NSURL *pictureUrl;
